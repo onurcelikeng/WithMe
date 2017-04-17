c953a8a79ba6a9164ea11b0f29d13c4f50c619b7
Onur Celik <onrcelik@outlook.com>
2017-04-17 13:47:42 +0300

    Configuration class is added.

---
+++ WithMe.Service/Entities/Configurations/UserConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WithMe.Service.Entities.Configurations
+{
+    public class UserConfiguration
+    {
+    }
+}
