88f0a8f02dd980d725732fe3aa96d8d88e935887
Onur Celik <onrcelik@outlook.com>
2017-04-17 14:29:35 +0300

    LoggingConfiguration class is updated.

--- WithMe.Service/Entities/Configurations/LoggingConfiguration.cs
+++ WithMe.Service/Entities/Configurations/LoggingConfiguration.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using WithMe.Service.Entities.Tables;
 
 namespace WithMe.Service.Entities.Configurations
 {
-    public class LoggingConfiguration
+    public class LoggingConfiguration : EntityTypeConfiguration<Logging>
     {
+        public LoggingConfiguration() : this("dbo")
+        {
+
+        }
+
+        public LoggingConfiguration(string schema)
+        {
+            ToTable(schema + ".LoggingTable");
+            HasKey(x => x.Id);
+
+            Property(x => x.Id).HasColumnName(@"Id").IsRequired().HasColumnType("int").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(x => x.Table).HasColumnName(@"Table").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+            Property(x => x.Operation).HasColumnName(@"Operation").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+            Property(x => x.CreateDate).HasColumnName(@"CreateDate").IsRequired().HasColumnType("datetime");
+
+        }
     }
 }
