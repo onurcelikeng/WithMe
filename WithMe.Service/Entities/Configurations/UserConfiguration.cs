ed0276ebd076a1d089ddb8e1e9674e82c274971a
Onur Celik <onrcelik@outlook.com>
2017-04-17 14:13:37 +0300

    UserConfiguration class is updated.

--- WithMe.Service/Entities/Configurations/UserConfiguration.cs
+++ WithMe.Service/Entities/Configurations/UserConfiguration.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using WithMe.Service.Entities.Tables;
 
 namespace WithMe.Service.Entities.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : EntityTypeConfiguration<User>
     {
+        public UserConfiguration() : this("dbo")
+        {
+
+        }
+
+        public UserConfiguration(string schema)
+        {
+            ToTable(schema + ".UserTable");
+            HasKey(x => x.Id);
+
+            Property(x => x.Id).HasColumnName(@"Id").IsRequired().HasColumnType("int").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(x => x.FirstName).HasColumnName(@"FirstName").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+            Property(x => x.LastName).HasColumnName(@"LastName").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+            Property(x => x.Email).HasColumnName(@"Email").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+            Property(x => x.Image).HasColumnName(@"Image").IsRequired().HasColumnType("nvarchar").HasMaxLength(40);
+
+        }
     }
 }
