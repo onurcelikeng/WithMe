dda89e278a539b70578fc17a02a2ee29d92adf70
Onur Celik <onrcelik@outlook.com>
2017-04-22 02:12:24 +0300

    get user facebook images is complated

--- WithMe.Service/Helpers/FacebookHelper.cs
+++ WithMe.Service/Helpers/FacebookHelper.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using WithMe.Service.Entities.Tables;
 using WithMe.Service.Models;
 using Newtonsoft.Json;
 
@@ -28,6 +27,29 @@ namespace WithMe.Service.Helpers
         {
             var fbuser = Client.Get("me", new { fields = "id,name,first_name,middle_name,last_name,email" });
             return JsonConvert.DeserializeObject<FacebookUserModel>(JsonConvert.SerializeObject(fbuser));
+        }
+
+        public async Task<byte[]> GetProfileImage(string userid)
+        {
+            using (HttpClient http = new HttpClient()
+            {
+                BaseAddress = new Uri("https://graph.facebook.com/")
+            })
+            {
+                try
+                {
+                    var data = await http.GetAsync("/" + userid + "/picture?heigth=2000&width=2000&access_token=" + Client.AccessToken);
+                    if (data != null && data.IsSuccessStatusCode)
+                    {
+                        return await data.Content.ReadAsByteArrayAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Trace.Fail(ex.Message);
+                }
+            }
+            return null;
         }
     }
 }
