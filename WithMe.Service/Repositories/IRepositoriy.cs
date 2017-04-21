65e086cf8d7ec5c30ee7f0ffaeb7ae5d7b5d444c
Onur Celik <onrcelik@outlook.com>
2017-04-21 17:29:59 +0300

    added irepository and repository classes.

---
+++ WithMe.Service/Repositories/IRepositoriy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WithMe.Service.Repositories
+{
+    interface IRepository<T> where T : class
+    {
+        List<T> List();
+
+        T Find(object id);
+
+        List<T> Where(Func<T, bool> where);
+
+        bool Add(T obj);
+
+        bool Update(T obj);
+
+        bool Delete(T obj);
+    }
+}
---
+++ WithMe.Service/Repositories/Repositoriy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using WithMe.Service.Entities;
+
+namespace WithMe.Service.Repositories
+{
+    public class Repository<T> : IDisposable, IRepository<T> where T : class
+    {
+        DataContext context;
+        DbSet<T> Table;
+
+
+        public Repository()
+        {
+            context = new DataContext();
+            Table = context.Set<T>();
+        }
+
+        ~Repository()
+        {
+            Dispose();
+        }
+
+
+        public DbSet<T> Context()
+        {
+            return Table;
+        }
+
+        public bool Add(T obj)
+        {
+            Table.Add(obj);
+            return Save();
+        }
+
+        public bool Delete(T obj)
+        {
+            Table.Remove(obj);
+            return Save();
+        }
+
+        public T Find(object id)
+        {
+            return Table.Find(id);
+        }
+
+        public List<T> List()
+        {
+            return Table.ToList();
+        }
+
+        public bool Update(T obj)
+        {
+            return Save();
+        }
+
+        public List<T> Where(Func<T, bool> where)
+        {
+            return Table.Where(where).ToList();
+        }
+
+        public T First(Func<T, bool> where)
+        {
+            return Table.FirstOrDefault(where);
+        }
+
+        private bool Save()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                //LogHelper.Log("Veritabanında işlem hatası: " + ex.Message, LogType.Error, ex);
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+    }
+}
