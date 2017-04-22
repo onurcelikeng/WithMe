49ccdde9cd9dc74593f608a2567b787b70db6930
Onur Celik <onrcelik@outlook.com>
2017-04-22 10:34:54 +0300

    hide web.config

---
+++ WithMe.Service/Storage/ConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace WithMe.Service.Storage
+{
+    public class ConnectionString
+    {
+        private string account = CloudConfigurationManager.GetSetting("StorageAccountName");
+        private string key = CloudConfigurationManager.GetSetting("StorageAccountKey");
+
+
+        public CloudStorageAccount GetConnectionString()
+        {
+            string connectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", account, key);
+            return CloudStorageAccount.Parse(connectionString);
+        }
+    }
+}
